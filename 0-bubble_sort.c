8825b63b7786eed6ec79cf24c354f324b8b4232f
prosperemebo <emeboprosper@gmail.com>
2023-07-24 02:11:05 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		swapped = 0;
 
-		for (j = 0; j < size - i - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
