d68403bc363f5739989e9d0dccb4a4c7db5f03ea
prosperemebo <emeboprosper@gmail.com>
2023-07-24 02:15:50 +0100

    New Push

--- print_list.c
+++ print_list.c
@@ -8,16 +8,16 @@
  */
 void print_list(const listint_t *list)
 {
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	int i;
+	i = 0;
+	
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
